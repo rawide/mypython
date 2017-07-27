77bfba7e08595da23fee0411e34b50b014df6ab9
sephy <sephy@ubuntu16.04>
2017-07-27 23:16:41 +0800

    init file

---
+++ SocialWealth/SocialWealth.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+import matplotlib.pyplot as plt
+import numpy as np
+print 'where the money going'
+
+x = [0, 100]
+y = random
+plt.figure()
+plt.title("round =")
+plt.xlabel("human number")
+plt.ylabel("human wealth")
+plt.plot(x, y)
+plt.show()
