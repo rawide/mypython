9e6873227479aaf47b30c4c727d90380c7563484
Sephy <sorcerer2002@163.com>
2017-08-07 23:53:48 +0800

    add social wealth init code

--- SocialWealth/SocialWealth.py
+++ SocialWealth/SocialWealth.py
@@ -1,13 +1,43 @@
 #! /usr/bin/python
 import matplotlib.pyplot as plt
 import numpy as np
-print 'where the money going'
+round = range(0, 365*(65-22))
+people = range(0, 100)
+money = [100 for i in people]
+LIABILITY = False # False True
+INCREASE_WEALTH = 'absolute' #absolute precent none
+#SUB_ALLOWANCE = True
 
-x = [0, 100]
-y = np.random.randint(1, 100, 100)
+'''
+for i in people:
+    test_rand = np.random.randint(1, 100, 100)
+    print(test_rand)
+'''
+for i in round:
+    index = np.random.randint(0, 100, 100)
+    for j in people:
+        if LIABILITY:
+            money[j] -= 1
+            money[index[j]] += 1
+        else:
+            if money[j] > 0:
+                money[j] -= 1
+                money[index[j]] += 1
+        if INCREASE_WEALTH == 'absolute':
+            if i % 365 == 0:
+                money[j] += sum(money)*0.02/100
+        elif INCREASE_WEALTH == 'precent':
+            if i % 365 == 0:
+                money[j] += money[j]*(0.02)
+
+money.sort()
 plt.figure()
-plt.title("round =")
-plt.xlabel("human number")
-plt.ylabel("human wealth")
-plt.hist(y, 100)
+plt.grid(True)
+#plt.title("round = %d" % round)
+plt.xlabel("player")
+plt.ylabel("wealth")
+plt.bar(people, money, width=1, facecolor='b', alpha=0.75)
+#print(money)
+plt.savefig('money.jpg')
 plt.show()
+#'''
